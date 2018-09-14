61f858184dc02971d475d7ddf480fc1956ae4196
Jenny K <jkam17@gmail.com>
2018-09-14 23:14:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears ORDER BY age WHERE alive = true"
+  "SELECT name, age FROM bears WHERE alive = true ORDER BY age"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1"
 end
 
 def selects_most_prominent_color_and_returns_with_count
